b7d2760215a5c43ddf4ac324818fcfc568ab1dd2
Jianyuan Wang <jianyuan@meta.com>
2025-05-14 03:57:55 +0100

    fix

--- demo.py
+++ demo.py
@@ -21,6 +21,8 @@ from pathlib import Path
 
 from vggt.models.vggt import VGGT
 from vggt.utils.load_fn import load_and_preprocess_images_square
+from vggt.utils.pose_enc import pose_encoding_to_extri_intri
+from vggt.utils.geometry import unproject_depth_map_to_point_map
 
 
 def parse_args():
