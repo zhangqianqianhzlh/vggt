0316128bddc498e8055a8b0fb7845469fa95dd2f
Jianyuan Wang <jianyuan@fb.com>
2025-06-01 17:29:34 -0700

    list todo for trainer

---
+++ training/trainer.py
@@ -0,0 +1,10 @@
+# Plan: This file should provide a trainer class, which provides
+# 1. The init of DDP
+# 2. The init of optimizers, tb, timers, and so on
+# 3. A basic training framework (especially for finetuning)
+#       self._train_epoch_
+#       self._process_batch_
+#       self._step_
+
+
+# ETA: hope I can finish this before June 8th
